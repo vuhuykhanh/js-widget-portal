a4ecac2072e99a4489373d015d4df3c89919998a
Vu Huy Khanh <k001534@cb.local>
2020-06-10 14:59:34 +0700

    1

--- js-widget.js
+++ js-widget.js
@@ -1,6 +1,8 @@
+var a1 = document.getElementById("1");
 var all = document.createElement("div");
 all.setAttribute("name","landorus");
 all.addEventListener("click",function(){
     alert("Hello");
 })
-document.body.appendChild(all);
+
+a1.appendChild(all);
