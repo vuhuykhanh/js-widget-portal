ebf2bedd13901dd1aed67b89106a13def80d885d
Vu Huy Khanh <k001534@cb.local>
2020-06-10 16:45:53 +0700

    3

--- js-widget.js
+++ js-widget.js
@@ -1,6 +1,7 @@
 var a1 = document.getElementById("1");
 var all = document.createElement("div");
 all.setAttribute("name","landorus");
+all.innerHTML = "Alo";
 all.addEventListener("click",function(){
     alert("Hello");
     all.innerHTML ="Saparangkangte";
